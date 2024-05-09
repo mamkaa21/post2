df790e81d4c039e7fe41a66147a76434ba4409df
mamka <mamka21a@mail.ru>
2024-05-09 16:52:29 +1000

    Добавьте файлы проекта.

---
+++ post2/model/AdressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace post2.model
+{
+    public class AdressBook
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public int ID_User { get; set; }
+    }
+}
---
+++ post2/model/Popemail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace post2.model
+{
+    public class Popemail
+    {
+        public Popemail()
+        {
+            Attachments = new List<Attachments>();
+            AdressBooks = new ObservableCollection<AdressBook>();
+        }
+        public int ID { get; set; }
+
+        public int ID_AdressFrom { get; set; }
+
+        public int ID_AdressTo { get; set; }
+
+        public int MessageNumber { get; set; }
+
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime DateSent { get; set; }
+
+        public List<Attachments> Attachments { get; set; }
+
+        public ObservableCollection<AdressBook> AdressBooks { get; set; }
+        public string EmailFrom { get; internal set; }
+        public string TitleFrom { get; internal set; }
+    }
+
+    [Serializable]
+    public class Attachments
+    {
+        public int ID { get; set; }
+
+        public byte[] Content { get; set; }
+
+        public string Title { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
+
